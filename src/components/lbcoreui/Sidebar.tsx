65eb49f6ee5630a878219d779d78d2f2aa8652ba
Joseph Ketterer <joseph.ketterer@littlebirdie.co.uk>
2025-06-04 16:45:29 +0100

    sidebar item

--- src/components/lbcoreui/Sidebar.tsx
+++ src/components/lbcoreui/Sidebar.tsx
@@ -1,60 +1,37 @@
 "use client";
 import Image from "next/image";
+import SidebarItem from "@/components/lbcoreui/SidebarItem";
 export default function Sidebar() {
   return (
     <div className="bg-lbgreen w-32 h-dvh flex flex-col">
       <nav>
         <ul className="flex flex-col min-h-[750px] justify-between">
           <div>
-          <li
-            className="p-6 flex flex-col items-center justify-center text-lbgreen border-1 border-r-1 border-lbgreen text-lg cursor w-full bg-white ease-in-out"
-            onClick={() => {}}
-          >
-            <Image src="/images/sidebar/Homegreen.svg" alt="list" width="32" height="32" />
-            Home
-          </li>
-          <li
-            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor w-full border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out"
-            onClick={() => {}}
-          >
-            <Image
-              src="/images/sidebar/List.svg"
-              alt="list"
-              width="32"
-              height="32"
-            />
-            Subs
-          </li>
-          <li
-            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out"
-            onClick={() => {}}
-          >
-            <Image src="/images/sidebar/Activity.svg" alt="list" width="32" height="32" />
-            Analytics
-          </li>
-          <li
-            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out"
-            onClick={() => {}}
-          >
-            <Image src="/images/sidebar/Dollar.svg" alt="list" width="32" height="32" />
-            Payments
-          </li>
-          <li
-            className=" p-6 flex flex-col items-center justify-center text-white text-lg cursor border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out "
-            onClick={() => {}}
-          >
-            <Image src="/images/sidebar/Tag.svg" alt="list" width="32" height="32" />
-            Marketplace
-          </li>
+          <SidebarItem
+            iconName="homegreen.svg"
+            label="Home"/>
+
+            
+           <SidebarItem
+            iconName="List.svg"
+            label="subs"/>
+            <SidebarItem
+            iconName="Activity.svg"
+            label="Analytics"/>
+             <SidebarItem
+            iconName="Dollar.svg"
+            label="Payments"/>
+
+            <SidebarItem
+            iconName="Tag.svg"
+            label="Marketplace"/>
+      
           </div>
           <div className="" >
-           <li
-            className=" p-6 flex flex-col items-center justify-center text-white text-lg cursor border-r-1 border-lbgreen hover:bg-white ease-in-out "
-            onClick={() => {}}
-          >
-            <Image src="/images/sidebar/help.svg" alt="list" width="32" height="32" />
-           Help
-          </li>
+            <SidebarItem
+            iconName="help.svg"
+            label="Help"/>
+          
           </div>
         </ul>
       </nav>
---
+++ src/components/lbcoreui/SidebarItem.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+type SidebarItemProps = {
+  iconName: string; // e.g., "Activity.svg"
+  label: string;
+};
+
+export default function SidebarItem({ iconName, label }: SidebarItemProps) {
+  const [hover, setHover] = useState(false);
+
+  const imageSrc = hover
+    ? `/images/sidebar/green/${iconName}`
+    : `/images/sidebar/${iconName}`;
+
+  return (
+    <li
+      className="p-6 flex flex-col items-center justify-center text-white text-lg cursor w-full border-1 border-r-1 border-lbgreen hover:bg-white hover:text-lbgreen ease-in-out"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Image src={imageSrc} alt={label} width={32} height={32} />
+      {label}
+    </li>
+  );
+}
