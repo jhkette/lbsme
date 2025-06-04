30b835fe909247e4f1307f5c28244ab6d2ffd576
Joseph Ketterer <joseph.ketterer@littlebirdie.co.uk>
2025-06-04 12:15:17 +0100

    image tidying

--- src/components/lbcoreui/Sidebar.tsx
+++ src/components/lbcoreui/Sidebar.tsx
@@ -7,50 +7,49 @@ export default function Sidebar() {
         <ul className="flex flex-col min-h-[750px] justify-between">
           <div>
           <li
-            className="p-6 flex flex-col items-center justify-center text-lbgreen text-lg cursor w-full bg-white ease-in-out"
+            className="p-6 flex flex-col items-center justify-center text-lbgreen border-1 border-r-1 border-lbgreen text-lg cursor w-full bg-white ease-in-out"
             onClick={() => {}}
           >
-            <Image src="/Homegreen.svg" alt="list" width="32" height="32" />
+            <Image src="/images/sidebar/Homegreen.svg" alt="list" width="32" height="32" />
             Home
           </li>
           <li
-            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor w-full hover:bg-white ease-in-out"
+            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor w-full border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out"
             onClick={() => {}}
           >
             <Image
-              src="/List.svg"
+              src="/images/sidebar/List.svg"
               alt="list"
               width="32"
               height="32"
-              className="bg-lbgreen"
             />
             Subs
           </li>
           <li
-            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor hover:bg-white ease-in-out"
+            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out"
             onClick={() => {}}
           >
-            <Image src="/Activity.svg" alt="list" width="32" height="32" />
+            <Image src="/images/sidebar/Activity.svg" alt="list" width="32" height="32" />
             Analytics
           </li>
           <li
-            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor hover:bg-white ease-in-out"
+            className="p-6 flex flex-col items-center justify-center text-white text-lg cursor border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out"
             onClick={() => {}}
           >
-            <Image src="/Dollar.svg" alt="list" width="32" height="32" />
+            <Image src="/images/sidebar/Dollar.svg" alt="list" width="32" height="32" />
             Payments
           </li>
           <li
-            className=" p-6 flex flex-col items-center justify-center text-white text-lg cursor hover:bg-white ease-in-out "
+            className=" p-6 flex flex-col items-center justify-center text-white text-lg cursor border-1 border-r-1 border-lbgreen hover:bg-white ease-in-out "
             onClick={() => {}}
           >
-            <Image src="/Tag.svg" alt="list" width="32" height="32" />
+            <Image src="/images/sidebar/Tag.svg" alt="list" width="32" height="32" />
             Marketplace
           </li>
           </div>
           <div className="" >
            <li
-            className=" p-6 flex flex-col items-center justify-center text-white text-lg cursor hover:bg-white ease-in-out "
+            className=" p-6 flex flex-col items-center justify-center text-white text-lg cursor border-r-1 border-lbgreen hover:bg-white ease-in-out "
             onClick={() => {}}
           >
             <Image src="/images/sidebar/help.svg" alt="list" width="32" height="32" />
