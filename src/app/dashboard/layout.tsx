a3b033158ba08179cf3b502fa51e5ecaf835a0ba
Joseph Ketterer <joseph.ketterer@littlebirdie.co.uk>
2025-06-06 19:10:27 +0100

    layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,12 @@
 "use client";
 import HeaderDashboard from "@/components/lbcoreui/HeaderDashboard";
 import Sidebar from "@/components/lbcoreui/Sidebar";
-
+import useApolloClient from "@/apollo/useApolloClient";
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const apolloClient = useApolloClient();
+  console.log("Apollo Client initialized:", apolloClient);
  
+  
   return (
     <div className="flex flex-col items-start  min-h-screen bg-gray-100">
       <HeaderDashboard />
