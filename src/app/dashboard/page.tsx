bc8bbc7960752a921b26d4467aed11f6d5872468
Joseph Ketterer <joseph.ketterer@littlebirdie.co.uk>
2025-06-09 19:46:47 +0100

    adding commented code for subscriptions

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 import DashboardSubs from "@/components/subscriptionsLanding/DashboardSubs";
-
+//import { format } from 'date-fns'; // add later
 import Image from "next/image";
 
 import IntroHome from "@/components/lbcoreui/IntroHome";
 export default function Home() {
 
+  // const {
+  //   data: spendingsData,
+  //   loading: spendingsLoading,
+  //   refetch: spendingsRefetch,
+  // } = useGetSubscriptionSpendingsQuery({
+  //   variables: {
+  //     from: formatDate(selectedDates.start),
+  //     to: formatDate(selectedDates.end),
+  //     category: userCategory.searchName,
+  //   },
+  //   fetchPolicy: "cache-and-network",
+  // });
+  
+  //const formatDate = (date: Date) => format(date, 'yyyy-MM-dd');
   return (
     <div className="px-16 w-full mt-12 relative">
       <IntroHome/>
