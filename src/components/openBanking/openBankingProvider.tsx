7c4fe6ba4aa430c6caecbb696cf05b48a555fd50
Joseph Ketterer <joseph.ketterer@littlebirdie.co.uk>
2025-07-03 08:18:24 +0100

    banking

--- src/components/openBanking/openBankingProvider.tsx
+++ src/components/openBanking/openBankingProvider.tsx
@@ -2,39 +2,41 @@
 import { useState, useEffect } from "react";
 import { useGetProvidersQuery } from "@/graphql/getProviders.generated";
 import { Search } from "lucide-react";
-
+import {ProviderResult} from "@/interfaces/ProviderResult";
 
 export default function OpenBankingProvider() {
-    const [search, setSearch] = useState("");
-    const [providers, setProviders] = useState([]);
-     const { loading, error, data, refetch } = useGetProvidersQuery({
-        variables: { name: "" },
-        errorPolicy: "all",
-        fetchPolicy: "cache-and-network",
-      });
-    useEffect(() => {
-        if (data?.getProviders) {
-            setProviders(data.getProviders);
+  const [search, setSearch] = useState("");
+  const [providers, setProviders] = useState<ProviderResult[]>([]);
+  const { loading, error, data, refetch } = useGetProvidersQuery({
+    variables: { name: "" },
+    errorPolicy: "all",
+    pollInterval: 10000,
+    fetchPolicy: "cache-and-network",
+  });
+  useEffect(() => {
+    if (data?.getProviders) {
+      setProviders(data.getProviders);
+    }
+  }, [data?.getProviders]);
+  console.log(data, "OPEN BANKING DATA");
+ 
 
-        }},[data?.getProviders]);
-      console.log(data, "OPEN BANKING DATA");
-
-      console.log(providers, "OPEN BANKING PROVIDERS");
+  console.log(providers, "OPEN BANKING PROVIDERS");
   return (
-       <div className="relative w-128 bg-white ">
-  {/* Search Icon */}
-  <Search
-    className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
-    size={20}
-  />
+    <div className="relative w-128 bg-white ">
+      {/* Search Icon */}
+      <Search
+        className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+        size={20}
+      />
 
-  {/* Input */}
-  <input
-    type="text"
-    placeholder="Filter by name or type..."
-    onChange={(e) => setSearch(e.target.value)}
-    className="pl-10 pr-4 py-2 border rounded w-full placeholder-gray-500"
-  />
-</div>
-  )
+      {/* Input */}
+      <input
+        type="text"
+        placeholder="Filter by name or type..."
+        onChange={(e) => setSearch(e.target.value)}
+        className="pl-10 pr-4 py-2 border rounded w-full placeholder-gray-500"
+      />
+    </div>
+  );
 }
---
+++ src/interfaces/ProviderResult.ts
@@ -0,0 +1,8 @@
+export interface ProviderResult {
+  __typename?: "ProviderResult";
+  providerId: string;
+  displayName: string;
+  icon?: string | null | undefined;
+  regions: string[]; // e.g., ["GBR"]
+  maintenanceStatus: boolean;
+}
