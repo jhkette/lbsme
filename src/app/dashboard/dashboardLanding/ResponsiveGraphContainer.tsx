1b775c3c8bbab5b7f34aecd8c59176c8ba53836e
Joseph Ketterer <joseph.ketterer@littlebirdie.co.uk>
2025-08-10 11:29:27 +0100

    changes

---
+++ src/app/dashboard/dashboardLanding/ResponsiveGraphContainer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import LineChartPayment from "./graphs/LineGraph";
+import BarchartGraph from "./graphs/BarchartGraph";
+export default function ResponsiveGraphContainer({
+  barchart,
+  simplifiedData,
+}: {
+  simplifiedData: { displayName: string; monthlyCost: number; fill: string }[];
+  barchart: boolean;
+}) {
+
+
+  // Sort data by monthlyCost descending
+  const sortedData = [...simplifiedData].sort(
+    (a, b) => b.monthlyCost - a.monthlyCost
+  );
+
+
+
+  return barchart ? (
+    <BarchartGraph simplifiedData={simplifiedData}/>
+  ) : (
+    <LineChartPayment/>
+  );
+}
+
---
+++ src/app/dashboard/dashboardLanding/TransactionDetail.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Subscription } from "@/interfaces/Subscription";
+import { useGetSubscriptionQuery } from "@/graphql/getSubscriptionDetail.generated";
+import Image from "next/image";
+import Circle from "@/components/lbcoreui/Circle";
+
+interface SpendingDetailProps {
+  sub: Subscription;
+  yearly: boolean;
+}
+
+export default function SpendingDetail(props: SpendingDetailProps) {
+  const { loading, error, data, refetch } = useGetSubscriptionQuery({
+    variables: { id: props.sub.subscriptionId },
+    // Optional: Add error policy
+    errorPolicy: "all",
+    // Optional: Cache policy
+    fetchPolicy: "cache-and-network",
+    // Optional: Poll for updates every 30 seconds
+    // pollInterval: 30000,
+  });
+
+  const now = new Date();
+
+  const currentYear = now.getFullYear();
+  const currentMonth = now.getMonth();
+
+  if (loading) {
+    return null;
+  }
+
+  const icon = data?.getSubscription.merchant.icon as string;
+
+
+
+  const totalThisMonth = (data?.getSubscription?.transactions ?? [])
+    .filter((tx): tx is NonNullable<typeof tx> => !!tx && !!tx.bookingTime)
+    .filter((tx) => {
+      const date = new Date(tx.bookingTime as string);
+      return (
+        date.getFullYear() === currentYear && date.getMonth() === currentMonth
+      );
+    })
+    .reduce(
+      (sum, tx) => sum + (tx.amount && tx.amount.amount ? tx.amount.amount : 0),
+      0
+    );
+  const isManual = data?.getSubscription?.isManual || false;
+  const renewalDate = new Date(data?.getSubscription?.dates.renewalDate || "");
+  const isAfterRenewal = now > renewalDate;
+
+  let displayAmount: string | null;
+
+  if (props.yearly) {
+    displayAmount = isManual ? null : `£${data?.getSubscription?.costs?.yearly.toFixed(2)}`;
+  } else if (data?.getSubscription?.isManual && isAfterRenewal) {
+    displayAmount = `£${data?.getSubscription?.costs?.monthly ?? "0.00"}`;
+  } else {
+    displayAmount = `£${totalThisMonth.toFixed(2)}`;
+  }
+
+  return (
+    <>
+      {displayAmount !== null && (
+        <div className="flex flex-row justify-between gap-4 py-2 border-b-1 items-center">
+          <div className="flex flex-row items-center justify-start gap-4">
+            {typeof icon === "string" && icon !== "unknown" && icon !== "" ? (
+              <Image
+                src={icon}
+                alt={data?.getSubscription.displayName as string}
+                width={50}
+                height={50}
+              />
+            ) : (
+              <Circle />
+            )}
+
+            <p className="hidden md:block bg-lbbgblue text-white px-3 py-1 rounded-lg text-xs">
+              {" "}
+              {data?.getSubscription.paymentMethod
+                ? data?.getSubscription.paymentMethod
+                : "Unknown"}{" "}
+            </p>
+            <div>
+              {data?.getSubscription.merchant.name &&
+              data.getSubscription.merchant.name.length > 40 ? (
+                <p className="text-xs block w-fit">
+                  {data.getSubscription.merchant.name}
+                </p>
+              ) : (
+                <p className="text-sm md:text-base block w-fit">
+                  {data?.getSubscription.merchant.name
+                    ? data.getSubscription.merchant.name
+                    : ""}
+                </p>
+              )}
+            </div>
+          </div>
+          <p className="text-lg font-semibold">{displayAmount}</p>
+        </div>
+      )}
+    </>
+  );
+}
