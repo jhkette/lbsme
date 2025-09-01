c3b292d36cdb3e6306d8c3e8e1ba05d5c97a9c5f
Joseph Ketterer <joseph.ketterer@littlebirdie.co.uk>
2025-09-01 09:19:07 +0100

    consts format file tidy

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -6,9 +6,7 @@ export const STORAGE_KEY = "userSignupCredentials";
 export const cancelSubscriptionLink =
   "https://www.littlebirdie.co.uk/cancellation-tips";
 
-
-
-  export const FD_LINK_BROADBAND =
+export const FD_LINK_BROADBAND =
   "https://bionic.co.uk/business-connectivity/business-phone-line/?r=brndbi&gclid=Cj0KCQjw_L_FBhDmARIsAItqgt59QzYM6YhcEYE162aGwZhLc3Kiui_wPGCq4eIQj5Pu5fsDOMkIA1caAoFEEALw_wcB";
 export const FD_LINK_MOBILE =
   "https://www.awin1.com/cread.php?awinmid=25756&awinaffid=1070447&ued=https%3A%2F%2Fwww.moneysupermarket.com%2Fmobile-phones%2F";
@@ -22,10 +20,10 @@ export const FD_LINK_PET_INS =
   "http://www.awin1.com/cread.php?awinmid=12049&awinaffid=1070447&clickref=&p=https%3A%2F%2Fwww.moneysupermarket.com%2Fpet-insurance%2F";
 export const FD_LINK_TRAVEL_INS =
   "https://www.awin1.com/cread.php?awinmid=12049&awinaffid=1070447&ued=https%3A%2F%2Fwww.moneysupermarket.com%2Ftravel-insurance%2F";
-export const FD_LINK_LIFE_INS = "https://www.moneysupermarket.com/life-insurance/";
-export const FD_LINK_FINANCE = "https://bionic.co.uk/business-finance/?r=brndbi&gclid=Cj0KCQjwxdXBBhDEARIsAAUkP6jzOrxTSZ5ZXQYKjhcQMUfErlny0JXmenKgGBvqsaFHPKE5UhE1ifIaAqtlEALw_wcB";
-
-
+export const FD_LINK_LIFE_INS =
+  "https://www.moneysupermarket.com/life-insurance/";
+export const FD_LINK_FINANCE =
+  "https://bionic.co.uk/business-finance/?r=brndbi&gclid=Cj0KCQjwxdXBBhDEARIsAAUkP6jzOrxTSZ5ZXQYKjhcQMUfErlny0JXmenKgGBvqsaFHPKE5UhE1ifIaAqtlEALw_wcB";
 
 export const FIXED_DEALS = [
   { sk: "SUBCATEGORY#PAYTV", url: "https://broadband.littlebirdie.co.uk" },
