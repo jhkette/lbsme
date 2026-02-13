954956f1392dd940f61249b48ca5b9f5f084953e
jhkette <joseph.ketterer@gmail.com>
2026-02-13 09:58:03 +0000

    mobile check improvement

--- src/components/mobileCheck/MobileCheck.tsx
+++ src/components/mobileCheck/MobileCheck.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 export default function MobileCheck({ children }: { children: ReactNode }) {
 	const router = useRouter();
+	const [isChecking, setIsChecking] = useState(true);
 
 	useEffect(() => {
 		const isMobile = () => {
@@ -13,10 +14,22 @@ export default function MobileCheck({ children }: { children: ReactNode }) {
 			return /android|iphone|ipad|ipod/i.test(ua);
 		};
 
-		if (isMobile()) {
-			router.push("/mobile-redirect");
-		}
+		// Small timeout to prevent flash of content
+		const timeout = setTimeout(() => {
+			if (isMobile()) {
+				router.push("/mobile-redirect");
+			} else {
+				setIsChecking(false);
+			}
+		}, 100); 
+
+		return () => clearTimeout(timeout);
 	}, [router]);
+
+	// Don't render children until we've checked for mobile
+	if (isChecking) {
+		return null; 
+	}
 
 	return <>{children}</>;
 }
